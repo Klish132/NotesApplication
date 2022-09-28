b4b45c769c697480cdda46cd5a6cfec6da5708a1
Klish132 <tuha99@mail.ru>
2022-09-29 02:06:59 +0500

    Test

--- NotesApplication/Program.cs
+++ NotesApplication/Program.cs
@@ -17,12 +17,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
     {
+        Console.WriteLine("Production");
         var m = Regex.Match(Environment.GetEnvironmentVariable("DATABASE_URL")!, @"postgres://(.*):(.*)@(.*):(.*)/(.*)");
         options.UseNpgsql($"Server={m.Groups[3]};Port={m.Groups[4]};User Id={m.Groups[1]};Password={m.Groups[2]};Database={m.Groups[5]};sslmode=Prefer;Trust Server Certificate=true");
     }
     else // In Development Environment
     {
         // So, use a local Connection
+        Console.WriteLine("Dev");
         options.UseNpgsql(builder.Configuration.GetConnectionString("NotesDB"));
     }
 });
